10888f9d38fba004a84d49fffb646d6a46951eca
YihaoGuo2018 <44004911+YihaoGuo2018@users.noreply.github.com>
2020-02-22 20:59:35 -0500

    Update server.py

--- server.py
+++ server.py
@@ -6,52 +6,129 @@ import json
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 #
-import complex_num
-import feedback
-import reading
-import simpledatabase
+import complex_num4
+import complex_num1
+import complex_num2
+import complex_num3
+
+
+import ask_question_1
+import ask_question_2
+
+import analyse_answer1
+import analyse_answer2
+
 import similarity_core
-import feedback_complexnumber
+
 
 core = similarity_core.similarity_core()
-tmp = reading.Reading(core)
-t2 = [3, 2, 3, 3, 3, 3, 3, 1, 2, 1, 3, 2, 1, 3, 2, 1, 3, 1, 2, 3, 2, 1, 3, 2, 3, 2, 1]
-t3 = simpledatabase.help1()
+ask_question_p1 = ask_question_1.ask_question_1(core)
+# t2 = [3, 2, 3, 3, 3, 3, 3, 1, 2, 1, 3, 2, 1, 3, 2, 1, 3, 1, 2, 3, 2, 1, 3, 2, 3, 2, 1]
+# t3 = simpledatabase.help1()
 
 
-@app.route('/getanswer', methods=['POST'])
-def hello_world():
+@app.route('/ask_question/p1', methods=['POST'])
+def q1():
     ttt = request.get_json()["question"]
 
 
 
-    number, question = tmp.classify(ttt)
-    print(number)
-    print(question)
-    if number == -1:
-        answer = "0"
-        answer += question
-        print("not find")
-        return answer
-    elif number == -2:
-        answer = "2"
-        answer += question
-        print("not find")
-        return answer
+    result = ask_question_p1.classify(ttt)
+    score1 = result[0][0]
+    score2 = result[1][0]
+    score3 = result[2][0]
 
-    id = number
-    answer = "1"
-    answer += t3[id][t2[id] + 1]
-    return answer
+    question1 = result[0][1]
+    question2 = result[1][1]
+    question3 = result[2][1]
+
+    answer1 = result[0][2]
+    answer2 = result[1][2]
+    answer3 = result[2][2]
+
+    out = {}
+    out["type"] = "3"
+    out["question1"] = question1
+    out["question2"] = question2
+    out["question3"] = question3
+    out["answer1"] = answer1
+    out["answer2"] = answer2
+    out["answer3"] = answer3
+
+
+    if score1 > 0.92:
+        out["type"] = "1"
+    elif score1 > 0.5:
+        out["type"] = "2"
+    out = json.dumps(out)
+    return out
 
 
 
-@app.route('/complexnumber', methods=['POST'])
+
+
+ask_question_p2 = ask_question_2.ask_question_2(core)
+
+@app.route('/ask_question/p2', methods=['POST'])
+def q2():
+    ttt = request.get_json()["question"]
+
+
+
+    result = ask_question_p2.classify(ttt)
+    score1 = result[0][0]
+    score2 = result[1][0]
+    score3 = result[2][0]
+
+    question1 = result[0][1]
+    question2 = result[1][1]
+    question3 = result[2][1]
+
+    answer1 = result[0][2]
+    answer2 = result[1][2]
+    answer3 = result[2][2]
+
+
+    out = {}
+    out["type"] = "3"
+    out["question1"] = question1
+    out["question2"] = question2
+    out["question3"] = question3
+    out["answer1"] = answer1
+    out["answer2"] = answer2
+    out["answer3"] = answer3
+
+    example1 = result[0][3]
+    example2 = result[1][3]
+    example3 = result[2][3]
+
+    example1_exist = "2" if example1 == "" else "1"
+    example2_exist = "2" if example2 == "" else "1"
+    example3_exist = "2" if example3 == "" else "1"
+
+    out["example1"] = example1
+    out["example2"] = example2
+    out["example3"] = example3
+
+    out["example1_exist"] = example1_exist
+    out["example2_exist"] = example2_exist
+    out["example3_exist"] = example3_exist
+
+
+    if score1 > 0.92:
+        out["type"] = "1"
+    elif score1 > 0.5:
+        out["type"] = "2"
+    out = json.dumps(out)
+    return out
+
+
+##复数除法
+
+@app.route('/complex_number/4', methods=['POST'])
 def hello_world2():
 
 
-
-    print(request.get_json())
     a = request.get_json()["A"]
     b = request.get_json()["B"]
     c = request.get_json()["C"]
@@ -65,32 +142,172 @@ def hello_world2():
     answer = request.get_json()["answer"]
 
 
-    division = complex_num.division(a, b, c, d)
+    division = complex_num4.division(a, b, c, d)
     division.set_step(step)
-    print("answer")
-    print(answer)
-    print("qustion")
-    print(division)
-    print("step")
-    print(division.stepnow)
-    print("back")
 
-    out = division.input(answer)
-    print(out)
+    content = division.input(answer)
     stepnow = division.stepnow
+    finish = division.is_finish()
 
-
-
+    type = 1
     if stepnow == step:
-        return "0" + str(stepnow) + out
+        type = 0
+    elif finish == 1:
+        type = 2
+
+    out = {}
+    out["type"] = str(type)
+    out["step"] = str(stepnow)
+    out["content"] = content
+
+
+    out = json.dumps(out)
+    return out
+
+##复数加法
+@app.route('/complex_number/1', methods=['POST'])
+def hello_world3():
+
+
+    a = request.get_json()["A"]
+    b = request.get_json()["B"]
+    c = request.get_json()["C"]
+    d = request.get_json()["D"]
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    step = request.get_json()["step"]
+    step = int(step)
+    answer = request.get_json()["answer"]
+
+
+    division = complex_num1.addition(a, b, c, d)
+    division.set_step(step)
+
+    content = division.input(answer)
+    stepnow = division.stepnow
+    finish = division.is_finish()
+
+    type = 1
+    if stepnow == step:
+        type = 0
+    elif finish == 1:
+        type = 2
+
+
+    out = {}
+    out["type"] = str(type)
+    out["step"] = str(stepnow)
+    out["content"] = content
+
+
+    out = json.dumps(out)
+    return out
+
+##复数减法
+
+@app.route('/complex_number/2', methods=['POST'])
+def hello_world4():
+
+
+    a = request.get_json()["A"]
+    b = request.get_json()["B"]
+    c = request.get_json()["C"]
+    d = request.get_json()["D"]
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    step = request.get_json()["step"]
+    step = int(step)
+    answer = request.get_json()["answer"]
+
+
+    division = complex_num2.subtraction(a, b, c, d)
+    division.set_step(step)
+
+    content = division.input(answer)
+    stepnow = division.stepnow
+    finish = division.is_finish()
+
+    type = 1
+    if stepnow == step:
+        type = 0
+    elif finish == 1:
+        type = 2
+
+
+    out = {}
+    out["type"] = str(type)
+    out["step"] = str(stepnow)
+    out["content"] = content
+
+
+    out = json.dumps(out)
+    return out
+
+##复数乘法
+@app.route('/complex_number/3', methods=['POST'])
+def hello_world5():
+
+
+    a = request.get_json()["A"]
+    b = request.get_json()["B"]
+    c = request.get_json()["C"]
+    d = request.get_json()["D"]
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    step = request.get_json()["step"]
+    step = int(step)
+    answer = request.get_json()["answer"]
+
+
+    division = complex_num3.multiplication(a, b, c, d)
+    division.set_step(step)
+
+    content = division.input(answer)
+    stepnow = division.stepnow
+    finish = division.is_finish()
+
+    type = 1
+    if stepnow == step:
+        type = 0
+    elif finish == 1:
+        type = 2
+
+
+    out = {}
+    out["type"] = str(type)
+    out["step"] = str(stepnow)
+    out["content"] = content
+
+
+    out = json.dumps(out)
+    return out
 
 
 
 
-    return "1" + str(stepnow) + out
 
 
-ff = feedback.feedback(core)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ff = analyse_answer1.analyse_answer1(core)
 
 @app.route('/feedback/question', methods=['get'])
 def getquestion():
@@ -102,17 +319,26 @@ def getquestion():
 
 
 
-@app.route('/feedback/checkanswer', methods=['post'])
+@app.route('/analyse_answer/a1', methods=['post'])
 def getanswer():
-    question_id = int(request.get_json()["question"])
+    question_id = int(request.get_json()["question_id"])
     inanswer = request.get_json()["answer"]
 
-    score, answer = ff.checkanswer(inanswer, question_id)
-    return str(score) + answer
+    score, question, answer = ff.checkanswer(inanswer, question_id)
+    out = {}
+    out["type"] = "3"
+    out["question"] = question
+    out["answer"] = answer
+    if score > 0.92:
+        out["type"] = "1"
+    elif score > 0.5:
+        out["type"] = "2"
+    out = json.dumps(out)
+    return out
 
 
 
-ff2 = feedback_complexnumber.feedback_CN(core)
+ff2 = analyse_answer2.analyse_answer2(core)
 
 
 @app.route('/feedback/CN/question', methods=['get'])
@@ -125,13 +351,112 @@ def getquestion2():
 
 
 
-@app.route('/feedback/CN/checkanswer', methods=['post'])
+@app.route('/analyse_answer/a2', methods=['post'])
 def getanswer2():
-    question_id = int(request.get_json()["question"])
+    question_id = int(request.get_json()["question_id"])
     inanswer = request.get_json()["answer"]
 
-    score, answer = ff2.checkanswer(inanswer, question_id)
-    return str(score) + answer
+    score, question, answer = ff2.checkanswer(inanswer, question_id)
+    print(score)
+    out = {}
+    out["type"] = "3"
+    out["question"] = question
+    out["answer"] = answer
+    if score > 0.85:
+        out["type"] = "1"
+    elif score > 0.5:
+        out["type"] = "2"
+    out = json.dumps(out)
+    return out
+
+
+
+
+## FOIL1
+import FOIL1
+@app.route('/FOIL/1', methods=['POST'])
+def FOIL1_f():
+
+
+    a = request.get_json()["A"]
+    b = request.get_json()["B"]
+    c = request.get_json()["C"]
+    d = request.get_json()["D"]
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    step = request.get_json()["step"]
+    step = int(step)
+    answer = request.get_json()["answer"]
+
+
+    division = FOIL1.foil1(a, b, c, d)
+    division.set_step(step)
+
+    content = division.input(answer)
+    stepnow = division.stepnow
+    finish = division.is_finish()
+
+    type = 1
+    if stepnow == step:
+        type = 0
+    elif finish == 1:
+        type = 2
+
+
+    out = {}
+    out["type"] = str(type)
+    out["step"] = str(stepnow)
+    out["content"] = content
+
+
+    out = json.dumps(out)
+    return out
+
+
+## FOIL2
+import FOIL2
+@app.route('/FOIL/2', methods=['POST'])
+def FOIL2_f():
+
+
+    a = request.get_json()["A"]
+    b = request.get_json()["B"]
+    c = request.get_json()["C"]
+    d = request.get_json()["D"]
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    step = request.get_json()["step"]
+    step = int(step)
+    answer = request.get_json()["answer"]
+
+
+    division = FOIL2.foil2(a, b, c, d)
+    division.set_step(step)
+
+    content = division.input(answer)
+    stepnow = division.stepnow
+    finish = division.is_finish()
+
+    type = 1
+    if stepnow == step:
+        type = 0
+    elif finish == 1:
+        type = 2
+
+
+    out = {}
+    out["type"] = str(type)
+    out["step"] = str(stepnow)
+    out["content"] = content
+
+
+    out = json.dumps(out)
+    return out
+
 
 
 
